0deb54e0f48d9ac3b076f007248ccb0d6675641b
Akihito Koriyama <akihito.koriyama@gmail.com>
2021-01-19 12:06:02 +0900

    Update AttributeReaderTest.php

--- tests-php8/AttributeReaderTest.php
+++ tests-php8/AttributeReaderTest.php
@@ -74,13 +74,13 @@ class AttributeReaderTest extends TestCase
 
     public function testGetAbstractAnnotation(): void
     {
-        $class = new ReflectionClass($this->target);
+        $class = new ReflectionClass(Fake::class);
         $classAnnotation = $this->reader->getClassAnnotation($class, AbstractFoo::class);
         $this->assertInstanceOf(AbstractFoo::class, $classAnnotation);
-        $method = new ReflectionMethod($this->target, 'setKey');
+        $method = new ReflectionMethod(Fake::class, 'setKey');
         $methodAnnotation = $this->reader->getMethodAnnotation($method, AbstractFoo::class);
         $this->assertInstanceOf(AbstractFoo::class, $methodAnnotation);
-        $prop = new ReflectionProperty($this->target, 'prop');
+        $prop = new ReflectionProperty(Fake::class, 'prop');
         $propAnnotation = $this->reader->getPropertyAnnotation($prop, AbstractFoo::class);
         $this->assertInstanceOf(AbstractFoo::class, $propAnnotation);
     }
